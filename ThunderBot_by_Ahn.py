b7b386f4c4fedf34d1916b85b3e40cda1e3d564d
ah2308 <92104505+ah2308@users.noreply.github.com>
2021-10-08 13:50:33 +0900

    Update ThunderBot_by_Ahn.py

--- ThunderBot_by_Ahn.py
+++ ThunderBot_by_Ahn.py
@@ -13,7 +13,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("실행준비 완료")
-    game = discord.Game("대기")
+    game = discord.Game("ThunderBot_1.1V")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
@@ -75,14 +75,30 @@ async def on_message(message):
         channel = message.channel
         await msg.add_reaction("👍")
 
-    def check(reaction, user):
-        return user == message.author and str(reaction.emoji) == '👍'
-    try:
-        reaction, user = await client.wait_for('reaction_add', timeout=3600.0, check=check)
-    except asyncio.TimeoutError:
-        await channel.send('❌'+" 참여 가능한 시간이 초과되었습니다.")
-    else:
-        await channel.send("💬"+ user.name + "님이(가) 참여를 희망합니다")
+    if message.content.startswith("!아브노말"):
+        await message.channel.send("@everyone")
+        embed = discord.Embed(title="아브렐슈드 노말 모집", timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00ff00)
+        embed.add_field(name=message.content[6:], value="참여를 원하시면 아래 👍이모지를 눌러주세요", inline=False)
+        embed.set_footer(text="ThunderBot by. 안태훈")
+        msg = await message.channel.send(embed=embed)
+        channel = message.channel
+        await msg.add_reaction("👍")
+        
+    if message.content.startswith("!아브하드"):
+        await message.channel.send("@everyone")
+        embed = discord.Embed(title="아브렐슈드 하드 모집", timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00ff00)
+        embed.add_field(name=message.content[6:], value="참여를 원하시면 아래 👍이모지를 눌러주세요", inline=False)
+        embed.set_footer(text="ThunderBot by. 안태훈")
+        msg = await message.channel.send(embed=embed)
+        channel = message.channel
+        await msg.add_reaction("👍")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if user.bot == 1: #봇이면 패스
+        return None
+    if str(reaction.emoji) == "👍":
+        await reaction.message.channel.send(user.name + "님이 step 아이템을 구매")
 
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
